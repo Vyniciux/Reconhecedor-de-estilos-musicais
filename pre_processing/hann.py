701e56888f50aebb7f5127b3f9209b463f3eb518
JJ <julnsilva20@gmail.com>
2025-03-24 15:18:25 -0300

    fix: resolve problemas na criação de diretórios dos pré-processamentos.

--- pre_processing/hann.py
+++ pre_processing/hann.py
@@ -3,28 +3,48 @@ import numpy as np
 import librosa
 import soundfile as sf
 
-input_base_path = "janelamento_output/forro"
+input_base_path = "janelamento_output"
 output_base_path = "hann_output"
 
-# Output file structure
-for root, dirs, files in os.walk(input_base_path):
-    for dir_name in dirs:
-        output_dir = root.replace(input_base_path, output_base_path)
-        os.makedirs(os.path.join(output_dir, dir_name), exist_ok=True)
+def solve_path(input_path):
+    caminho_relativo = os.path.relpath(input_path, input_base_path)
+    genre, song, window = caminho_relativo.split(os.sep)
+    window = os.path.splitext(window)[0]  # Remover extensão
+        
+    log_mel_directory = os.path.join(output_base_path, genre, song)
+    os.makedirs(log_mel_directory, exist_ok=True)
 
-# Iterates over all files
-for root, _, files in os.walk(input_base_path):
-    for file in files:
-        if file.endswith(".wav"):
-            input_path = os.path.join(root, file)
-            output_path = input_path.replace(input_base_path, output_base_path)
+    output_dir = os.path.join(output_base_path, genre, song, window)
+    return output_dir
 
-            # load the audio 20ms window
-            y, sr = librosa.load(input_path, sr=None)
 
-            # Applies Hann
-            hann_window = np.hanning(len(y))
-            y_hann = y * hann_window
+def hann(input_path):
+    # Iterates over all files
+    output_path = solve_path(input_path) + ".wav"
 
-            sf.write(output_path, y_hann, sr)
-            print(f"Processado: {output_path}")
+    # load the audio 20ms window
+    y, sr = librosa.load(input_path, sr=None)
+
+    # Applies Hann
+    hann_window = np.hanning(len(y))
+    y_hann = y * hann_window
+
+    sf.write(output_path, y_hann, sr)
+    print(f"Processado: {output_path}")
+
+
+# path to genre folder
+folder_path = "janelamento_output/rock"
+
+#Iterates over all the windows in a single song for testing purposes
+# for window in os.listdir(folder_path):
+#     if window.endswith(".wav"):
+#         wav_file_path = os.path.join(folder_path, window).replace("\\", "/")
+#         hann(wav_file_path)
+
+# Iterantes over all the songs in a genre dir
+for song in os.listdir(folder_path):
+    song_folder_path = os.path.join(folder_path, song)
+    for window in os.listdir(song_folder_path):
+        wav_file_path = os.path.join(folder_path, song, window).replace("\\", "/")
+        hann(wav_file_path)
--- pre_processing/janelamento.py
+++ pre_processing/janelamento.py
@@ -3,14 +3,22 @@ import numpy as np
 import os
 from scipy.io import wavfile
 
-def janela_audio(arquivo_audio, pasta_saida="janelamento_output", duracao_janela_ms=20, sobreposicao=0.5):
-    #caminho do arquivo
-    caminho_relativo = os.path.relpath(arquivo_audio, "../data")
-    genero, nome_musica = caminho_relativo.split(os.sep)[:2]
-    nome_musica = os.path.splitext(nome_musica)[0]  # Remover extensão
-    
-    pasta_musica = os.path.join(pasta_saida, genero, nome_musica)
-    os.makedirs(pasta_musica, exist_ok=True)
+input_base_path = "../data"
+output_base_path = "janelamento_output"
+
+def solve_path(input_path):
+    caminho_relativo = os.path.relpath(input_path, input_base_path)
+    genre, song = caminho_relativo.split(os.sep)
+    song = os.path.splitext(song)[0]  # Remover extensão
+        
+    log_mel_directory = os.path.join(output_base_path, genre, song)
+    os.makedirs(log_mel_directory, exist_ok=True)
+    output_dir = os.path.join(output_base_path, genre, song)
+    return output_dir
+
+
+def janela_audio(arquivo_audio, duracao_janela_ms=20, sobreposicao=0.5):
+    pasta_musica = solve_path(arquivo_audio)
     
     y, sr = librosa.load(arquivo_audio, sr=None)
     
@@ -30,10 +38,15 @@ def janela_audio(arquivo_audio, pasta_saida="janelamento_output", duracao_janela
     return janelas, sr
 
 # path to genre folder
-folder_path = "../data/forro"  # Substitua pelo caminho da sua pasta
+folder_path = "../data/rock"  # Substitua pelo caminho da sua pasta
 
-# Iterates over all the elements
-for filename in os.listdir(folder_path):
-    if filename.endswith(".wav"):
-        mp3_file_path = os.path.join(folder_path, filename)
-        janelas, sr = janela_audio(mp3_file_path)
+# Iterates over all the elements in the directory
+# for filename in os.listdir(folder_path):
+#     if filename.endswith(".wav"):
+#         wav_file_path = os.path.join(folder_path, filename).replace("\\", "/")
+#         #print(wav_file_path)
+#         janelas, sr = solve_path(wav_file_path)
+
+
+# Only one song for test purposes
+janelas, sr = janela_audio("../data/rock/Cabrobró.wav")
